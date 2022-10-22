1cb9ec2b75b3304cba9a49f411ee8f9ffec9c531
Hasibur Rahman <hasibur.rahman@vigaet.com>
2022-10-22 12:34:39 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./Components/Common/Header/Header";
 import Home from "./Pages/Home/Home";
 
 function App() {
+  const name = "";
   return (
     <div className="App">
       <Header />
