4933119da38020ec97e6b801128faf36fc29d1cd
Hasibur Rahman <hasibur.rahman@vigaet.com>
2022-10-22 23:25:34 +0600

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Common/Footer/Footer";
 import Header from "./Components/Common/Header/Header";
 import Home from "./Pages/Home/Home";
@@ -6,8 +7,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
-      <p>Hello</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
